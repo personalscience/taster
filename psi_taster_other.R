bee3fbef7d68002b620a5baa6eaa0490e1bb4bfa
Richard Sprague <richardsprague@gmail.com>
2021-09-07 19:57:22 -0700

    manually adding new users and data to the database. TODO: simplify this and consolidate it with the other db read files.

--- psi_taster_other.R
+++ psi_taster_other.R
@@ -1,8 +1,49 @@
 # load additional Tastermonial data files
 
+# This is hard-coded to read specific files.
+# DO NOT RUN MORE THAN ONCE
+
+library(psiCGM)
+
+Sys.setenv(R_CONFIG_ACTIVE = "shinyapps")
+
+conn_args <- config::get("dataconnection")
+con <- DBI::dbConnect(
+  drv = conn_args$driver,
+  user = conn_args$user,
+  host = conn_args$host,
+  port = conn_args$port,
+  dbname = conn_args$dbname,
+  password = conn_args$password
+)
+
+
+nutrisense_andreos <- glucose_df_from_nutrisense(filepath = file.path(config::get("tastermonial")$datadir,
+                                                                     "nutrisense_andreos.csv"),
+                                                user_id = 1006)
+
 nutrisense_sherri <- glucose_df_from_nutrisense(filepath = file.path(config::get("tastermonial")$datadir,
-                                                       "1627653419114_export-SherriJo.csv"))
+                                                       "1627653419114_export-SherriJo.csv"),
+                                                user_id = 1005)
 
 nutrisense_anthony <- glucose_df_from_nutrisense(filepath = file.path(config::get("tastermonial")$datadir,
                                                        "nutrisense_anthony_davis.csv"),
-                                  tz = Sys.timezone())
+                                  tz = Sys.timezone(),
+                                  user_id = 1021)
+
+new_nutrisense_records <- bind_rows(nutrisense_sherri,
+                                    nutrisense_anthony
+                                    )
+
+anthony_record <- tibble(first_name = "Anthony", last_name = "Davis", birthdate=as.Date("1900-01-01"),
+                         libreview_status = NA,
+                         user_id = 1021)
+
+
+if(nrow(tbl(con, "user_list") %>% filter(user_id ==1021) %>% collect()) > 0) message("new user already exists") else {
+
+DBI::dbWriteTable(con, "user_list", anthony_record, append = TRUE )
+DBI::dbWriteTable(con, "glucose_records", new_nutrisense_records, append = TRUE )
+DBI::dbWriteTable(con, "notes_records",  notes_df_from_glucose_table(user_id = 1005), append = TRUE)
+
+}
