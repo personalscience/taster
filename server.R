54015cd2af633e8cd6160e72c2a916bd98e4e9e6
Richard Sprague <richardsprague@gmail.com>
2021-09-09 16:41:13 -0700

    prettier chart to show comparisons

--- server.R
+++ server.R
@@ -37,7 +37,7 @@ server <- function(input, output) {
 
    g <- mod_libreview_plotServer("modChart", active_glucose_record, title = username)
 
-   food_time_ggplot <- mod_foodTasterServer("food_compare_plot", active_glucose_record, title = username )
+   mod_foodTasterServer("food_compare_plot", active_glucose_record, title = username )
 
 
 }
