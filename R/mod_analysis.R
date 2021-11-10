4987a4492e2bb71158be9233adae27558436528a
Richard Sprague <richardsprague@gmail.com>
2021-11-10 07:48:37 -0800

    Significant speed improvements by waiting till analytics are wanted before computing.

--- R/mod_analysis.R
+++ R/mod_analysis.R
@@ -49,9 +49,17 @@ mod_analysis_server <- function(id, glucose_df, con, GLUCOSE_RECORDS, NOTES_RECO
     ns <- session$ns
 
 
-    auc_df <- build_all_AUC(s_list = food_list_db(),
+    auc_df <- reactive({
+      validate(
+        need(input$calc,"Press Calculate to see Analytics")
+      )
+
+
+      build_all_AUC(s_list = food_list_db(),
                             glucose_records = GLUCOSE_RECORDS,
                             notes_records = NOTES_RECORDS)
+    }
+    )
 
 
     AUC_for_food <- function(foodname) {
@@ -59,9 +67,14 @@ mod_analysis_server <- function(id, glucose_df, con, GLUCOSE_RECORDS, NOTES_RECO
                          notes_records = NOTES_RECORDS)
     }
 
-    all_foods <- tibble(foodname = food_list_db() ,
+    all_foods <- reactive({
+      validate(
+        need(input$calc,"Press Calculate to see Analytics")
+      )
+      tibble(foodname = food_list_db() ,
                         iAUC = purrr::map_dbl(food_list_db(), function(x) {AUC_for_food(x) %>% pull(iAUC) %>% mean()}),
                         n = purrr::map_dbl(food_list_db(), function(x) {AUC_for_food(x) %>% nrow()}))
+    })
 
     output$boxplot <- renderPlot({
 
@@ -69,18 +82,19 @@ mod_analysis_server <- function(id, glucose_df, con, GLUCOSE_RECORDS, NOTES_RECO
         need(input$calc,"Press Calculate to see Analytics")
       )
 
-        auc_df %>% tidyr::pivot_longer(cols=ave:iAUC, names_to = "param") %>%
+
+        auc_df() %>% tidyr::pivot_longer(cols=ave:iAUC, names_to = "param") %>%
 
         filter(param == "iAUC") %>%
         ggplot(aes(x=foodname, y=value)) +
         geom_boxplot()+
         # geom_point( aes(x=foodname,y=value), color = "red") +
-        geom_point( data = auc_df %>% filter(user_id == input$user_id), aes(x=foodname, y = iAUC), color = "blue", size = 4) +
+        geom_point( data = auc_df() %>% filter(user_id == input$user_id), aes(x=foodname, y = iAUC), color = "blue", size = 4) +
         #geom_facet(rows=vars(user_id)) +
         labs(title = "Tastermonial Product Test Results", x = "", y = "iAUC Among All Testers") +
         psi_theme()})
 
-    output$food_summary <- renderTable({all_foods})
+    output$food_summary <- renderTable({all_foods() %>% arrange(iAUC)}, digits = 0)
   })
 }
 
