c0c994249262e2305799ad0d2476f7acc0a93cfa
Richard Sprague <richardsprague@gmail.com>
2021-11-30 11:59:01 -0800

    ask basic questions

--- R/mod_registration.R
+++ R/mod_registration.R
@@ -12,6 +12,7 @@ mod_registration_ui <- function(id){
   tagList(
     h1("Registration page"),
     p("Answers to all questions are optional"),
+    uiOutput(ns("questions")),
     actionButton(ns("reg_save"), label = "Save")
 
   )
@@ -24,8 +25,17 @@ mod_registration_server <- function(id, user){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    output$questions <- renderUI({
+      tagList(
+        textInput(ns("first_name"), label = "First Name"),
+        textInput(ns("last_name"), label = "Last Name"),
+        numericInput(ns("age_roughly"), value = 25, label = "Your Age (roughly)")
+      )
+    })
+
     observeEvent(input$reg_save, {
       message("Thanks for saving!")
+      message(sprintf("first_name=%s, last_name=%s, age=%d\n", input$first_name, input$last_name, input$age_roughly))
     })
 
   })
