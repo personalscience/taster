82c63fbe7457d544320bc1022d67d083231923a9
Richard Sprague <richardsprague@gmail.com>
2021-12-02 10:55:31 -0800

    Browser App Title = "Personal Science CGM"

--- R/app_ui.R
+++ R/app_ui.R
@@ -89,7 +89,7 @@ golem_add_external_resources <- function(){
     favicon(),
     bundle_resources(
       path = app_sys('app/www'),
-      app_title = 'tastermonial'
+      app_title = 'Personal Science CGM'
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
